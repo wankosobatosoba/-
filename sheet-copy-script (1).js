16b951a4cdec17368a237244d981edafe1ce3fbd
wankosobatosoba <32akakitune@gmail.com>
2024-09-13 15:24:16 +0900

    Update sheet-copy-script (1).js

--- sheet-copy-script (1).js
+++ sheet-copy-script (1).js
@@ -3,18 +3,32 @@ function filterAndMoveData() {
   var sheetA = ss.getSheetByName("A");
   var sheetB = ss.getSheetByName("B");
   
-  // AシートからデータをコピーAColumnName
-  var dataRange = sheetA.getRange("A7:BR7").getValues()[0];
+  // ヘッダー行を取得（6行目）
+  var headersA = sheetA.getRange(6, 1, 1, sheetA.getLastColumn()).getValues()[0];
+  var headersB = sheetB.getRange(6, 1, 1, sheetB.getLastColumn()).getValues()[0];
+
+  // 必要なカラムのインデックスを取得
+  var colIndices = {
+    status: headersA.indexOf("状態"),
+    bsColumn: headersB.indexOf("BS"),
+    sortColumn: headersB.indexOf("ソート基準")
+  };
+
+  // データ行を取得（7行目）
+  var dataRange = sheetA.getRange(7, 1, 1, sheetA.getLastColumn()).getValues()[0];
   
-  // E列（4番目）の値がBSAX撮去かどうかをチェック
-  if (dataRange[4] === "PSAX撤去") {
+  // 状態が "PSAX撤去" かどうかをチェック
+  if (dataRange[colIndices.status] === "PSAX撤去") {
     // Bシートに貼り付け
-    sheetB.getRange(7, 1, 1, dataRange.length).setValues([dataRange]);
+    var targetRange = sheetB.getRange(7, 1, 1, dataRange.length);
+    targetRange.setValues([dataRange]);
     
-    // BS列（71番目）が "o" のものだけにフィルター
-    if (dataRange[70] === "o") {
-      // G列（6番目）で昇順にソート
-      sheetB.getRange("A7:BR").sort({column: 7, ascending: true});
+    // BSカラムが "o" のものだけにフィルター
+    var bsCell = targetRange.offset(0, colIndices.bsColumn);
+    if (bsCell.getValue() === "o") {
+      // ソート基準カラムで昇順にソート
+      var sortRange = sheetB.getRange(7, 1, sheetB.getLastRow() - 6, sheetB.getLastColumn());
+      sortRange.sort({column: colIndices.sortColumn + 1, ascending: true});
     }
   }
 }
