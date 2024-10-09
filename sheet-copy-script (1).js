e52368d4f877ba44778bc080ccbd779f3b6298bf
wankosobatosoba <32akakitune@gmail.com>
2024-10-09 18:25:18 +0900

    Update sheet-copy-script (1).js

--- sheet-copy-script (1).js
+++ sheet-copy-script (1).js
@@ -1,29 +1,37 @@
-function manipulateSheets() {
-  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var sheetA = spreadsheet.getSheetByName('A');
-  var sheetB = spreadsheet.getSheetByName('B');
+function copySheetWithFormatting() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheetA = ss.getSheetByName("A");
+  var sheetB = ss.getSheetByName("B");
   
-  // Aシートからデータを取得
-  var dataA = sheetA.getRange(7, 1, sheetA.getLastRow() - 6, 70).getValues(); // A列からBR列まで
-  
-  // F列が"PSAX撤去"のデータのみをフィルター
-  var filteredData = dataA.filter(function(row) {
-    return row[5] === 'PSAX撤去'; // F列は0から数えて5番目
-  });
-  
-  // Bシートのデータをクリア
-  sheetB.getRange(7, 1, sheetB.getLastRow() - 6, 70).clearContent();
-  
-  // フィルターしたデータをBシートに貼り付け
-  if (filteredData.length > 0) {
-    sheetB.getRange(7, 1, filteredData.length, 70).setValues(filteredData);
+  if (!sheetA || !sheetB) {
+    Logger.log("シートAまたはシートBが見つかりません。");
+    return;
   }
   
-  // BS列でフィルター
-  var range = sheetB.getRange(7, 1, sheetB.getLastRow() - 6, 70);
-  var filter = range.getFilter() || range.createFilter();
-  filter.setColumnFilterCriteria(71, SpreadsheetApp.newFilterCriteria().whenTextEqualTo('o')); // BS列は71番目
+  var newSheetA = ss.insertSheet("C_A");
+  var newSheetB = ss.insertSheet("C_B");
   
-  // G列で昇順に並べ替え
-  range.sort({column: 7, ascending: true}); // G列は7番目
+  copySheet(sheetA, newSheetA);
+  copySheet(sheetB, newSheetB);
+  
+  Logger.log("コピーが完了しました。");
+}
+
+function copySheet(sourceSheet, targetSheet) {
+  var range = sourceSheet.getDataRange();
+  var numRows = range.getNumRows();
+  var numCols = range.getNumColumns();
+  
+  // 値と書式をコピー
+  range.copyTo(targetSheet.getRange(1, 1, numRows, numCols), {contentsOnly: false, formatOnly: false});
+  
+  // 列幅をコピー
+  for (var i = 1; i <= numCols; i++) {
+    targetSheet.setColumnWidth(i, sourceSheet.getColumnWidth(i));
+  }
+  
+  // 行の高さをコピー
+  for (var i = 1; i <= numRows; i++) {
+    targetSheet.setRowHeight(i, sourceSheet.getRowHeight(i));
+  }
 }
